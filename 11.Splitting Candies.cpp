e7dc2fc450ca9a54ad8a4882ff6c0b1af8037f00
Saloni Laddha <saloniladdha@microsoft.com>
2021-10-18 19:20:16 +0530

    Solution to codechef problem SPCANDY

---
+++ 11.Splitting Candies.cpp
@@ -0,0 +1,15 @@
+//Solution to CodeChef Problem code: SPCANDY
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    long long n,k;
+	    cin>>n>>k;
+	    if(k!=0) cout<<n/k<<" "<<n%k<<"\n";
+	    else cout<<k<<" "<<n<<"\n";
+	}
+	return 0;
+}
